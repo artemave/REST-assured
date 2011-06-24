40c5090c41cb4caeffbf9ed8249d0472855a041b
artemave <artemave@gmail.com>
2011-06-24 23:25:06 +0100

    clean accidently commited stuff

--- lib/fake_rest_services/models/fixture.rb
+++ lib/fake_rest_services/models/fixture.rb
@@ -1,29 +1,2 @@
 class Fixture < ActiveRecord::Base
 end
-
-if $0 == __FILE__
-  require 'test/unit'
-
-  class TestFixtureModel < Test::Unit::TestCase
-    def setup
-      AddFixtureTable.up
-      @fixture = Fixture.new
-    end
-    
-    def teardown
-      AddFixtureTable.down
-    end
-
-    def test_fixture_attributes_exist
-      [:url, :content].each do |method|
-        assert_respond_to @fixture, method, "#{@fixture.class.name} should have instance method called #{method.to_s}"
-      end
-    end
-
-    def test_fixture_class_methods_exist
-      [:create].each do |method|
-        assert_respond_to Fixture, method, "#{@fixture.class.name} should have class method called #{method.to_s}"
-      end
-     end
-  end
-end
