e050c83ec559f744ac049cc845968043a9d7da13
artemave <artemave@gmail.com>
2011-06-28 13:09:22 +0100

    - Get rid of gem extension hack (to run migrations on gem install) - instead, run migrations every time server starts. - Use ActiveRecord::Migrator instead of hacking around rake tasks (which     btw doesn't work if run form anywhere other than project root)

--- lib/fake_rest_services/config.rb
+++ lib/fake_rest_services/config.rb
@@ -1,11 +1,11 @@
 require 'sinatra/activerecord'
 require 'fake_rest_services/options'
-require 'rake'
-require 'sinatra/activerecord/rake'
 
 ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: FakeRestServices::Options.database || ':memory:'
 )
-Rake::Task['db:migrate'].reenable
-Rake::Task['db:migrate'].invoke
+
+ActiveRecord::Migrator.migrate(
+  File.expand_path('../../../db/migrate', __FILE__)
+)
