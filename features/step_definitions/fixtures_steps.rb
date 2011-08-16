6f81e05a85a81c207a5e02931db6d9f8d066b5e3
artemave <artemave@gmail.com>
2011-08-16 11:57:51 +0100

    steps files reorganized

--- features/step_definitions/fixtures_steps.rb
+++ features/step_definitions/fixtures_steps.rb
@@ -1,3 +1,49 @@
+# REST api steps
+
+When /^I create a fixture with "([^"]*)" as url and "([^"]*)" as response content$/ do |url, content|
+  post '/fixtures', { url: url, content: content }
+  last_response.should be_ok
+end
+
+Then /^there should be (#{CAPTURE_A_NUMBER}) fixture with "([^"]*)" as url and "([^"]*)" as response content$/ do |n, url, content|
+  Fixture.count.should == n
+  Fixture.where(url: url, content: content).count.should == 1
+end
+
+Given /^there is fixture with "([^"]*)" as url and "([^"]*)" as response content$/ do |url, content|
+  Fixture.create(url: url, content: content)
+end
+
+Given /^I register "([^"]*)" as url and "([^"]*)" as response content$/ do |url, content|
+  post '/fixtures', { url: url, content: content }
+  last_response.should be_ok
+end
+
+When /^I request "([^"]*)"$/ do |url|
+  get url
+end
+
+Then /^I should get "([^"]*)" in response content$/ do |content|
+  last_response.body.should == content
+end
+
+Given /^there are some fixtures$/ do
+  [['url1', 'content1'], ['url2', 'content2'], ['url3', 'content3']].each do |fixture|
+    Fixture.create(url: fixture[0], content: fixture[1])
+  end
+end
+
+When /^I delete all fixtures$/ do
+  delete '/fixtures/all'
+  last_response.should be_ok
+end
+
+Then /^there should be no fixtures$/ do
+  Fixture.count.should == 0
+end
+
+# UI steps
+
 Given /^the following fixtures exist:$/ do |fixtures|
   fixtures.hashes.each do |row|
     Fixture.create(url: row['url'], description: row['description'], content: row['content'])
