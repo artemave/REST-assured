2271f589ac69985e5c017f6389ca1c0653c34923
artemave <artemave@gmail.com>
2011-12-22 11:02:25 +0000

    PortExplorer.port_in_use? implemented

--- lib/rest-assured/utils/port_explorer.rb
+++ lib/rest-assured/utils/port_explorer.rb
@@ -1,13 +1,21 @@
-require 'socket'
+#require 'socket'
 
 module RestAssured
   module Utils
     class PortExplorer
-      def self.free_tcp_port
-        server = TCPServer.new('127.0.0.1', 0)
-        free_port = server.addr[1]
-        server.close
-        free_port
+      #def self.free_tcp_port
+        #server = TCPServer.new('127.0.0.1', 0)
+        #free_port = server.addr[1]
+        #server.close
+        #free_port
+      #end
+
+      def self.port_in_use?(port)
+        Net::HTTP.new('localhost', port).head('/')
+      rescue Errno::ECONNREFUSED
+        true
+      else
+        false
       end
     end
   end
--- spec/port_explorer_spec.rb
+++ spec/port_explorer_spec.rb
@@ -4,5 +4,19 @@ require File.expand_path('../../lib/rest-assured/utils/port_explorer', __FILE__)
 module RestAssured::Utils
   describe PortExplorer do
     it 'finds unused tcp port'
+
+    it 'knows if port is in use' do
+      port = 4566
+      Net::HTTP.should_receive(:new).with('localhost', port).and_raise(Errno::ECONNREFUSED)
+
+      PortExplorer.port_in_use?(port).should == true
+    end
+
+    it 'knows that port is NOT in use' do
+      port = 4566
+      Net::HTTP.should_receive(:new).with('localhost', port).and_return(connection = double.as_null_object)
+
+      PortExplorer.port_in_use?(port).should == false
+    end
   end
 end
