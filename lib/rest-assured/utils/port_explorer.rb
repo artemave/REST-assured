926600990a018b4ce5562fec166602e8948a462b
artemave <artemave@gmail.com>
2011-12-28 18:01:55 +0000

    PortExplorer specs rewritten and are now actually useful. Also 'Do NOT mock external libraries' is a very useful piece of advice

--- lib/rest-assured/utils/port_explorer.rb
+++ lib/rest-assured/utils/port_explorer.rb
@@ -1,21 +1,21 @@
-#require 'socket'
+require 'socket'
 
 module RestAssured
   module Utils
     class PortExplorer
-      #def self.free_tcp_port
-        #server = TCPServer.new('127.0.0.1', 0)
-        #free_port = server.addr[1]
-        #server.close
-        #free_port
-      #end
+      def self.free_tcp_port
+        server = TCPServer.new('127.0.0.1', 0)
+        free_port = server.addr[1]
+        server.close
+        free_port
+      end
 
       def self.port_in_use?(port)
-        Net::HTTP.new('localhost', port).head('/')
-      rescue Errno::ECONNREFUSED
-        true
+        Net::HTTP.get('127.0.0.1', '/', port)
+      rescue => e
+        !e.is_a?(Errno::ECONNREFUSED)
       else
-        false
+        true
       end
     end
   end
