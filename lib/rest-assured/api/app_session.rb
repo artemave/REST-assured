3495bc40e455e09d622d64c8da57fde6184b69b8
artemave <artemave@gmail.com>
2012-01-10 18:34:46 +0000

    AppSession spec

--- lib/rest-assured/api/app_session.rb
+++ lib/rest-assured/api/app_session.rb
@@ -1,12 +1,23 @@
 require 'rest-assured/utils/subprocess'
+require 'rest-assured/utils/drb_sniffer'
 require 'rest-assured/api/app_runner'
+require 'childprocess'
 
 module RestAssured
   class AppSession
+    include Utils::DrbSniffer
+
     def initialize
-      @child = Utils::Subprocess.new do
-        AppRunner.run!
-      end
+      @child = if drb?
+                 child = ChildProcess.build('rest-assured', *Config.to_cmdargs)
+                 child.io.inherit!
+                 child.start
+                 child
+               else
+                 Utils::Subprocess.new do
+                   AppRunner.run!
+                 end
+               end
     end
 
     def method_missing(*args)
--- spec/api/app_session_spec.rb
+++ spec/api/app_session_spec.rb
@@ -1,19 +1,43 @@
+require 'childprocess'
 require File.expand_path('../../spec_helper', __FILE__)
 require File.expand_path('../../../lib/rest-assured/api/app_session', __FILE__)
+require File.expand_path('../../../lib/rest-assured/utils/drb_sniffer', __FILE__)
 
 module RestAssured
   describe AppSession do
     context 'starts application' do
       it 'in subprocess if current process does NOT involve Drb (e.g. spork)' do
+        AppSession.any_instance.stub(:drb? => false)
+        state = ''
         Utils::Subprocess.should_receive(:new) do |&block|
+          state << 'called from block'
           block.call
+          state.clear
         end
-        AppRunner.should_receive(:run!)
+        AppRunner.should_receive(:run!) do
+          state.should == 'called from block'
+        end
 
         AppSession.new
       end
 
-      it 'in childprocess if current process relies on Drb (e.g. using spork)'
+      it 'in childprocess if current process relies on Drb (e.g. using spork)' do
+        AppSession.any_instance.stub(:drb? => true)
+        cmdargs = %w{-d :memory: -p 6666}
+        Config.stub(:to_cmdargs => cmdargs)
+
+        ChildProcess.should_receive(:build).with('rest-assured', *cmdargs).and_return(child = mock(:io => mock))
+
+        state = ''
+        child.io.should_receive(:inherit!) do
+          state.should_not == 'started'
+        end
+        child.should_receive(:start) do
+          state << 'started'
+        end
+
+        AppSession.new
+      end
     end
   end
 end
