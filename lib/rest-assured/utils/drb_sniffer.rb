8aaf66f7fddf76ede453d35e4718af2d535188a0
artemave <artemave@gmail.com>
2012-01-19 18:41:28 +0000

    DrbSniffer complete

--- lib/rest-assured/utils/drb_sniffer.rb
+++ lib/rest-assured/utils/drb_sniffer.rb
@@ -1,8 +1,11 @@
+require 'drb'
+
 module RestAssured
   module Utils
     module DrbSniffer
-      def drb?
-        true
+      def drb? # stolen from rspec
+        (DRb.current_server rescue false) &&
+          !!(DRb.current_server.uri =~ %r{druby://127\.0\.0\.1:})
       end
     end
   end
--- spec/drb_sniffer_spec.rb
+++ spec/drb_sniffer_spec.rb
@@ -1,5 +1,6 @@
 require File.expand_path('../spec_helper', __FILE__)
 require File.expand_path('../../lib/rest-assured/utils/drb_sniffer', __FILE__)
+require 'drb'
 
 module RestAssured::Utils
   describe DrbSniffer do
@@ -9,10 +10,24 @@ module RestAssured::Utils
       o
     end
 
-    it 'lets extendee to find out whether current process is using drb' do
-      extendee.drb?.should == true
+    context 'with DRb' do
+      before do
+        DRb.start_service("druby://127.0.0.1:#{PortExplorer.free_port}")
+      end
+      after do
+        DRb.stop_service
+      end
+
+      it 'lets extendee to find out whether current process is using drb' do
+        extendee.drb?.should == true
+      end
     end
-    it 'lets extendee to find out whether current process is NOT using drb'
+
+    context 'without DRb' do
+      it 'lets extendee to find out whether current process is NOT using drb' do
+        extendee.drb?.should == false
+      end
+    end
   end
 end
 
