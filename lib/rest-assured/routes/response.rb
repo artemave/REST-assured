a97e14d2f1a77e821687de8cfe6932a841a27878
はく <haku@vaguehope.com>
2012-02-13 19:40:08 +0000

    Redirect locally if there is a local double.

--- lib/rest-assured/routes/response.rb
+++ lib/rest-assured/routes/response.rb
@@ -14,20 +14,16 @@ module RestAssured
         app.body d.content
         app.status d.status
       elsif redirect_url = Models::Redirect.find_redirect_url_for(request.fullpath)
-        if !(redirect_url =~ /^http/)
-          if  d = Models::Double.where(:fullpath => redirect_url, :active => true, :verb => request.request_method).first
-            request.body.rewind
-            body = request.body.read #without temp variable ':body = > body' is always nil. mistery
-            env  = request.env.except('rack.input', 'rack.errors', 'rack.logger')
+        if d = Models::Double.where(:fullpath => redirect_url, :active => true, :verb => request.request_method).first
+          request.body.rewind
+          body = request.body.read #without temp variable ':body = > body' is always nil. mistery
+          env  = request.env.except('rack.input', 'rack.errors', 'rack.logger')
 
-            d.requests.create!(:rack_env => env.to_json, :body => body, :params => request.params.to_json)
+          d.requests.create!(:rack_env => env.to_json, :body => body, :params => request.params.to_json)
 
-            app.headers d.response_headers
-            app.body d.content
-            app.status d.status
-          else
-            app.redirect redirect_url
-          end
+          app.headers d.response_headers
+          app.body d.content
+          app.status d.status
         else
           app.redirect redirect_url
         end
